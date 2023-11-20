a2fd1f320a02bf60a438fea073e42ab1bc316c6b
pcih <pcih1984@gmail.com>
2023-11-20 19:36:29 +0300

    lesson_3

---
+++ lesson_3/src/main/java/homework/task002/tdd/User.java
@@ -0,0 +1,29 @@
+package homework.task002.tdd;
+
+public class User {
+    String name;
+    String password;
+
+    Role userRole;
+
+    boolean isAuthenticate = false;
+
+    public User(String name, String password, Role userRole) {
+        this.name = name;
+        this.password = password;
+        this.userRole = userRole;
+    }
+
+
+    public boolean authenticate(String name, String password) {
+        this.isAuthenticate = (this.name.equals(name) && this.password.equals(password));
+        return this.isAuthenticate;
+    }
+
+
+    
+    public boolean isRoleAdmin() {
+        return this.userRole.equals(Role.ADMIN);
+    }
+
+}
